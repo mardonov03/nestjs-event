58481c7c9c493ceed8bf602cf20ce0266d4084d0
nestjs event <mardonovk233@gmail.com>
2026-04-29 23:11:12 +0500

    need to create mailer module 001

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,33 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          host: 'smtp.gmail.com',
+          port: 587,
+          secure: false,
+          auth: {
+            user: config.get<string>('MAIL_USER'),
+            pass: config.get<string>('MAIL_PASS'),
+          },
+        },
+      }),
+    }),
+
     UsersModule,
     AuthModule,
+    MailerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { DtoSignUp } from './dto/dto.signUp';
 import { UsersService } from '../users/users.service';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userservice: UsersService) {}
+  constructor(
+    private readonly userservice: UsersService,
+    private readonly mailer: MailerService,
+  ) {}
+
   async signUp(dto: DtoSignUp) {
     const mail = dto.mail;
     const user = await this.userservice.getUserByMail(mail);
@@ -18,6 +23,14 @@ export class AuthService {
     ) {
       return 'еще не прошло 2 минуты';
     }
+    await this.sendMail(dto.mail);
     return await this.userservice.addUser(dto.mail, dto.password);
+  }
+  async sendMail(mail: string) {
+    await this.mailer.sendMail({
+      to: mail,
+      subject: 'test',
+      text: 'test text',
+    });
   }
 }
